e458474f60deaf6b94815e9afc5e3ace17cbf91c
Uwimana Jessy Bandya <jessy.bandya5@gmail.com>
2022-05-27 14:16:16 +0300

    done with splash screen

---
+++ app/src/main/java/com/example/e_jaby/SplashScreenActivity.kt
@@ -0,0 +1,21 @@
+package com.example.e_jaby
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+
+class SplashScreenActivity : AppCompatActivity() {
+
+    private val splashScreentimeout : Long = 3500
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+     Handler().postDelayed({
+
+                           startActivity(Intent(this, MainActivity::class.java))
+                           finish()
+     }, splashScreentimeout)
+    }
+}
